1fea97ff81364ae7d08470a43c4120620bcf9833
shaktisinh80880 <129297106+shaktisinh80880@users.noreply.github.com>
2023-04-15 20:21:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", 12345))
     API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5971569334:AAHpMaAQirVTcFgIObedb3HyTnFCHMcESy8")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", -100))
